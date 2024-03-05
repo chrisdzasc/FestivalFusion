124594d55c8c8b5dc4fdfeb054ea6ae99a27adbc
Christian Diaz <christian.dzasc@gmail.com>
2024-03-05 16:50:46 -0600

    Se añadió nuevas tareas en el gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
+const { src, dest, watch } = require('gulp'); //Entonces, src sirve para identificar un archivo y dest sirve para guardarlo.
+const sass = require("gulp-sass")(require('sass')); // Manda a llamar la función SASS
 
-function tarea( callback){
-    console.log("Mi primer tarea");
+function css(done){
 
-    callback();
+    // Identificar el archivo SASS
+    // Compilarlo
+    // Almacenarla en el disco duro
+
+    src('src/scss/app.scss') //Toma la ubicación de donde está el archivo
+        .pipe( sass() ) // Le aplica SASS
+        .pipe(dest('build/css')) // Lo guarda en el disco duro
+
+    done(); // Callback que avisa a gulp cuando llegamos al final de la ejecución.
 }
 
-exports.primerTarea = tarea;
+function dev(done){ // Va a ser una funciona que vamos a ejecutar algunas funciones con el watch
+
+    watch('src/scss/app.scss', css) // Cuando cambie la hoja de estilo, manda a llamar la función css
+
+    done()
+}
+
+exports.css = css; // Aqui mandamos llamar a la función CSS.
+exports.dev = dev; // Aqui mandamos llamar a la función dev.
