4d6edeaca9cd6be51dd164a35da5fd215b15f3b9
Christian Diaz <christian.dzasc@gmail.com>
2024-03-11 13:20:37 -0600

    Agregando video y metodo para convertir imagenes a webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-const { src, dest, watch } = require('gulp'); //Entonces, src sirve para identificar un archivo y dest sirve para guardarlo.
+const { src, dest, watch, parallel} = require('gulp'); //Entonces, src sirve para identificar un archivo y dest sirve para guardarlo.
+
+// CSS
 const sass = require("gulp-sass")(require('sass')); // Manda a llamar la función SASS
 const plumber = require('gulp-plumber'); // Instalamos la dependencia, extraemos la variable y la podemos utilizar
 
@@ -16,6 +18,22 @@ function css(done){
     done(); // Callback que avisa a gulp cuando llegamos al final de la ejecución.
 }
 
+async function versionWebp(done) {
+     
+    const webp = await import("gulp-webp"); // Manda a traer la dependencia instalada con "npm install --save-dev gulp-webp" desde la terminal"
+ 
+ 
+    const opciones = {
+        quality: 50 // Esto define que tanta calidad se le bajarán a las imágenes
+    }
+ 
+    src('src/img/**/*.{png,PNG,jpg,JPG}') // Busca recursivamente en todos los archivos y carpetas de la carpeta img con los formatos especificados
+        .pipe(webp.default(opciones)) // Los convierte en formato WEBP y les baja la calidad especificada
+        .pipe(dest('build/img')) // Los guarda en una nueva carpeta
+    
+    done(); // Callback que avisa a gulp cuando llegamos al final de la ejecución del script
+}
+
 function dev(done){ // Va a ser una funciona que vamos a ejecutar algunas funciones con el watch
 
     watch('src/scss/**/*.scss', css) // Cuando cambie la hoja de estilo, manda a llamar la función css
@@ -24,4 +42,5 @@ function dev(done){ // Va a ser una funciona que vamos a ejecutar algunas funcio
 }
 
 exports.css = css; // Aqui mandamos llamar a la función CSS.
-exports.dev = dev; // Aqui mandamos llamar a la función dev.
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev); // Aqui mandamos llamar a la función dev.
